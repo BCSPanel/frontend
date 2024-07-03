2124ba605228825a5fc5a5edd7d7d626cd7c4a3a
bddjr <bddjrcn@outlook.com>
2024-07-03 13:31:27 +0800

    简化logout

--- src/net/logout.ts
+++ src/net/logout.ts
@@ -1,6 +1,1 @@
-const form = document.createElement('form')
-form.action = './api/login/logout'
-form.style.display = 'none'
-
-/** 避免浏览器地址栏建议访问退出登录 */
-export default () => document.body.appendChild(form).submit()
+export default () => location.replace('./api/login/logout')
