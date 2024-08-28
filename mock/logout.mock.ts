7a5fbf660e5f2fe6d6fc0656fb9fed80b8eade06
bddjr <bddjrcn@outlook.com>
2024-08-28 21:45:04 +0800

    1

--- mock/logout.mock.ts
+++ mock/logout.mock.ts
@@ -1,7 +1,7 @@
 import { defineMock } from 'vite-plugin-mock-dev-server'
 
 export default defineMock({
-    url: '/api-login/logout',
+    url: '/api/login/logout',
     status: 303,
     headers: {
         location: '/'
--- src/App.vue
+++ src/App.vue
@@ -33,4 +33,46 @@ import Menu from '@/Menu.vue'
   </el-container>
 </template>
 
-<style src="./assets/App.scss"></style>
+<style lang="scss">
+* {
+  font-family: system-ui;
+}
+
+.el-container {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  .el-aside {
+    width: 140px;
+    height: 100%;
+    background-color: hsl(210, 100%, 100%, 93%);
+  }
+
+  .el-main {
+    padding: 12px;
+    /* margin: 8px; */
+    transition: var(--el-transition-duration);
+    /* overflow-y: hidden; */
+
+    .el-card {
+      --el-card-border-radius: 12px;
+      /* transition: width 0s; */
+    }
+  }
+
+  .el-footer {
+    --el-footer-padding: 0;
+  }
+}
+
+html.dark {
+  .el-container {
+    .el-aside {
+      background-color: hsla(0, 0%, 0%, 50%);
+    }
+  }
+}
+</style>
