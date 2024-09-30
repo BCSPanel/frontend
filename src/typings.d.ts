e082c9cc241d27a15761f74a25a3467e4fc2932f
bddjr <bddjrcn@outlook.com>
2024-09-30 21:45:57 +0800

    document.lastChild

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,6 +2,10 @@ declare module '*.svg';
 declare module '*.png';
 declare module '*.css';
 
+declare interface Document {
+    lastChild: HTMLHtmlElement
+}
+
 declare interface Window {
     supportES2023: boolean
 
