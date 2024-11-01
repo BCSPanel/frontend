7529206f8f286704672e1c13e18df777cd33595a
bddjr <bddjrcn@outlook.com>
2024-11-01 11:17:44 +0800

    20241101

---
+++ farm.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from '@farmfe/core';
+import viteCompression from 'vite-plugin-compression';
+
+export default defineConfig({
+  plugins: [
+    '@farmfe/plugin-react',
+  ],
+  vitePlugins: [
+    viteCompression({ algorithm: "brotliCompress" }),
+    viteCompression({ algorithm: "gzip" }),
+    {
+      name: 'afterbuild',
+      apply: 'build',
+      transformIndexHtml(html: string) {
+        return html.replace(/\n\s*/g, '')
+      },
+    },
+  ],
+  compilation: {
+    input: {
+      index: "index.html",
+      login: "login.html",
+    },
+    output: {
+      targetEnv: 'browser-esnext',
+      publicPath: './',
+      filename: "assets/[resourceName].[ext]",
+      assetsFilename: "assets/[resourceName].[ext]",
+    },
+    partialBundling: {
+      targetConcurrentRequests: 1,
+    },
+    runtime: {
+      isolate: true,
+    },
+    sourcemap: false,
+    comments: false,
+    progress: false,
+  },
+});
