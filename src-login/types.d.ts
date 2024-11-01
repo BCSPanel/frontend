4c84f4a9af088426ac59282d1054d14724f4a6fe
bddjr <bddjrcn@outlook.com>
2024-11-01 16:06:59 +0800

    tsconfig

--- src-login/types.d.ts
+++ src-login/types.d.ts
@@ -10,17 +10,11 @@ declare interface Window {
 
     imgBCSPanel: HTMLImageElement
     unsupportedES2023?: HTMLParagraphElement
-    welcome: HTMLParagraphElement
-
-    formLoginMode: HTMLDivElement
-    inputModeLogin: HTMLInputElement
-    inputModeReg: HTMLInputElement
 
     formLogin: HTMLDivElement
     inputUserName: HTMLInputElement
     inputPassword: HTMLInputElement
-    inputRepeatPassword: HTMLInputElement
-    inputVerifyCode: HTMLInputElement
+
     pStatus: HTMLParagraphElement
     submit: HTMLButtonElement
 }
