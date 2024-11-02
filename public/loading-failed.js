d8d4244961988766e4ee8032749976d0cea3f7db
bddjr <bddjrcn@outlook.com>
2024-11-02 11:56:09 +0800

    optimize loading-failed.js

--- public/loading-failed.js
+++ public/loading-failed.js
@@ -11,31 +11,36 @@ window.onerror = function (event, source, lineo, colno) {
 	if (window.root && !window.loading) return
 	window.stop && stop()
 
-	var innerHTML = '<h1>:(</h1><h2>Loading failed'
-	if (window.supportES2023) innerHTML += '</h2><p>Support'
-	else innerHTML += ', please upgrade or change browser</h2><p>Unsupported'
-	innerHTML += ' ES2023</p><style>body{background-color:#2977e7;margin:32px;color:white;font-family:Arial}h1{font-size:80px}h2{margin:48px 0;font-size:30px}</style>'
+	var output = '<h1>:(</h1><h2>Loading failed'
 
-	var b
-	if (b = /(MSIE |(Firefox|Chrome|Safari|Opera)\/)[\d\.]+/.exec(navigator.userAgent))
-		b = b[0]
+	if (window.supportES2023)
+		output += '</h2><p>Support'
+	else
+		output += ', please upgrade or change browser</h2><p>Unsupported'
+
+	output += ' ES2023</p><style>body{background-color:#2977e7;margin:32px;color:white;font-family:Arial}h1{font-size:80px}h2{margin:48px 0;font-size:30px}</style>'
+
+	var browserName
+	if (browserName = /(MSIE |(Firefox|Chrome|Safari|Opera)\/)[\d\.]+/.exec(navigator.userAgent))
+		browserName = browserName[0]
 	else if (navigator.userAgent.indexOf('Trident') > -1)
-		b = 'IE 11'
-	else b = 'unknown'
-	if (navigator.userAgent.indexOf('Mobi') > -1) b += ' Mobile'
+		browserName = 'IE 11'
+	else
+		browserName = 'unknown'
 
-	innerHTML += '<p>Browser: ' + b + '</p><p>UserAgent: ' + navigator.userAgent +
+	if (navigator.userAgent.indexOf('Mobi') > -1) browserName += ' Mobile'
+
+	output += '<p>Browser: ' + browserName + '</p><p>UserAgent: ' + navigator.userAgent +
 		'</p><p style="margin-top:48px">' +
-		event.replace(/&/g, '&amp;').replace(/</g, '&lt;') + '<br>' +
-		source.replace(location.protocol + '//' + location.host, '') + ' (' + lineo + ', ' + colno + ')</p>'
+		String(event).replace(/&/g, '&amp;').replace(/</g, '&lt;') + '<br>' +
+		String(source).replace(location.protocol + '//' + location.host, '') + ' (' + lineo + ', ' + colno + ')</p>'
 
 	function f() {
 		document.close()
-		document.write(innerHTML)
+		document.write(output)
 	}
-	for (var i = 100; i < 600; i += 100) {
+	for (var i = 100; i <= 1000; i += 100) {
 		setTimeout(f, i)
 	}
-	document.addEventListener("DOMContentLoaded", f)
 	f()
 }
